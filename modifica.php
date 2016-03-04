837bcde16ed67f713344bed4d19c3269f2be1d0e
cristhoper <cristhoper@lenovo>
2016-03-04 14:33:29 +0100

    segudo ejercicio examen-php2R

--- modifica.php
+++ modifica.php
@@ -1,58 +1,35 @@
 <?php
 session_start();
+//VARIABLES DE FORMULARIO
 include 'config.php';
-//VARIABLES DEL FORMULARIO
-$anyo = $_REQUEST['anyo'];
-$mes = $_REQUEST['mes'];
-$dia = $_REQUEST['dia'];
-$fecha = $anyo . "-" . $mes ."-" . $dia;
+$name2 = $_REQUEST['nombre'];
+$owner2 = $_REQUEST['duenyo'];
+$species2 = $_REQUEST['especie'];
+$sex2 = $_REQUEST['sexo'];
+$fecha = $_SESSION['fecha'];
+//VARIABLES DE SESION (comprobar si no se ha modificado ningun campo)
+$name = $_SESSION['name'];
+$owner = $_SESSION['owner'];
+$species = $_SESSION['species'];
+$sex = $_SESSION['sex'];
+
 
 //CONEXION A LA BD
 $conex = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 if($conex->connect_errno){
     die("Error al conectarnos a la base de datos: ".$conex->connect_errno);
 }
-//EVITAMOS INYECCION
-$safe_fecha = mysqli_real_escape_string($conex, $fecha);
 
-//SENTENCIA SQL
-$sql = "SELECT * FROM pet WHERE birth = '$safe_fecha' ";
-
-//Enviamos consulta
-$result = $conex->query($sql);
-
-//Comprobamos resultados
-if(($anyo=='') || ($mes=='') || ($dia=='')){
-    echo 'No ha introducido alguno de los datos';
+//Si el campo esta vacio 
+if(($name2=='') || ($sex2=='') || ($owner2=='') || ($species2=='')){
+    echo "Uno de los campos está vacio";
 }
-elseif($result->num_rows==0){
-    echo "No existen datos para la fecha que ha introducido";
+    
+// Si no se ha modificado ningun dato no se ejecutara el programa
+if(($name2==$name) && ($sex2==$sex) && ($owner2==$owner) && ($species2==$species))
+    echo "No ha modificado ningun campo";
+else {
+   $sql2 = "UPDATE `pet` SET `name`= '$name2',`owner`= '$owner2',`species`= '$species2',`sex`='$sex2' WHERE birth = '$fecha'";
+$result2 = $conex->query($sql2);          
+ 
 }
-elseif($result->num_rows>0){
-    while ($row = $result->fetch_assoc()) {
-//Asignamos nuevas variables para hacer mas sencillo el formulario
-        $name = $row['name'];
-        $owner = $row['owner'];
-        $species = $row['species'];
-        $sex = $row['sex'];
-        $birth = $row['birth'];
-    }       
-echo "<form method='post' action=modifica.php>"
-        . "Nombre:<input name='nombre' type=text value='$name'/> </br>"
-        . "Duenyo:<input name='duenyo' type=text value='$owner'/> </br>"
-        . "Especie:<input name='especie' type=text value='$species'/> </br>"
-        . "Sexo:<select name='sexo'>"
-        . "<option>m</option>"
-        . "<option>f</option>"
-        . " </select></br>"
-        . "Cumpleanyos:<input type=text value='$birth' readonly/> </br>"
-        . "<input type='submit' name='enviar'>";
-
-
-}
-//VAMOS A GUARDAR ESTAS VARIABLES PARA USARLAS MAS ADELANTE
-$_SESSION['name'] = $name;
-$_SESSION['owner'] = $owner;
-$_SESSION['species'] = $species;
-$_SESSION['sex'] = $sex;
-$_SESSION['fecha'] = $fecha;
